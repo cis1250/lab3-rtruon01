b02e9f7c187b046819782b36e0f66fb8c50a2099
rtruon01 <145353290+rtruon01@users.noreply.github.com>
2025-09-30 11:37:20 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python3
 
+counter = 0
+while counter <= 0:
+  print("User Input: ")
+  counter = int(input())
+  if counter > 0:
+    break
+  else:
+    print("\nPlease input a positive integer")
+
+print("Expected Output: ")
+a = 0
+b = 0
+c = 1
+while a < counter:
+  print(b)
+  temp = b + c
+  b = c
+  c = temp
+  a += 1
+  
 # Fibonacci Sequence Exercise
 # TODO: (Read detailed instructions in the Readme file)
 # Prompt the user for the number of terms.
